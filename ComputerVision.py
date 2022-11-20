9afa5131a1bf4be5340df0cf46f1c0550dcecc34
Marco Kaniecki <marcokaniecki@gmail.com>
2022-11-20 17:25:12 -0400

    updated comments and let user input file path

--- ComputerVision.py
+++ ComputerVision.py
@@ -9,45 +9,31 @@ from PIL import Image
 import sys
 import time
 
-'''
-Authenticate
-Authenticates your credentials and creates a client.
-'''
+
+# Authenticates credentials and creates a client
 subscription_key = "adb3838b97db400f9c5326e36e2d7037"
 endpoint = "https://marcoscomputervision.cognitiveservices.azure.com/"
 
 computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
-'''
-END - Authenticate
-'''
 
-'''
-Quickstart variables
-These variables are shared by several examples
-'''
-# Images used for the examples: Describe an image, Categorize an image, Tag an image, 
-# Detect faces, Detect adult or racy content, Detect the color scheme, 
-# Detect domain-specific content, Detect image types, Detect objects
-images_folder = os.path.join (os.path.dirname(os.path.abspath(__file__)), "images")
-remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
-'''
-END - Quickstart variables
-'''
 
-file_path = "/Users/marcokaniecki/Desktop/ECED4900-SYP/Sample-Data/286454219_5412071062164274_1507514144798269009_n.jpg"
+# images_folder = os.path.join (os.path.dirname(os.path.abspath(__file__)), "images")
 
-with open(file_path, mode='rb') as image_stream:
-    # Call API with remote image
-    results = computervision_client.describe_image_in_stream(image_stream)
-    # tags_result_remote = computervision_client.tag_image(remote_image_url )
+file_path = str(input("Enter path of image: "))
 
-    # Print results with confidence score
-    print("Tags in the remote image: ")
-    if (len(results.tags) == 0):
-        print("No tags detected.")
-    else:
-        for tag in results.tags:
-            print(tag)
-        #for tag in results.tags:
-            #print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
-    print()
+try:
+    with open(file_path, mode='rb') as image_stream:
+        # Call API with image
+        results = computervision_client.describe_image_in_stream( image_stream )
+        # tags_result_remote = computervision_client.tag_image(remote_image_url)
+
+        # Print results with extracted tags
+        print("===== Tags in the remote image =====")
+        if (len(results.tags) == 0):
+            print("No tags detected.")
+        else:
+            for tag in results.tags:
+                print(tag)
+        print()
+except FileNotFoundError:
+    print(f"{os.path.basename(file_path)} file not found")
