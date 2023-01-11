bedf663394e18cc8364eb5772af437b5e8592367
Marco Kaniecki <marcokaniecki@gmail.com>
2023-01-11 15:46:38 -0400

    added sample data and import features. Added looping through multiple images in a folder

--- ComputerVision.py
+++ ComputerVision.py
@@ -9,6 +9,11 @@ from PIL import Image
 import sys
 import time
 
+# island reffering to kitchen island
+important_features = [
+    "wood", "furniture", "table", "kitchen", "island", "bedroom", "bed", "porch", "stone",
+    "Driveway", "sidewalk", "basement"
+]
 
 # Authenticates credentials and creates a client
 subscription_key = "adb3838b97db400f9c5326e36e2d7037"
@@ -19,21 +24,28 @@ computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredenti
 
 # images_folder = os.path.join (os.path.dirname(os.path.abspath(__file__)), "images")
 
-file_path = str(input("Enter path of image: "))
+# file_path = str(input("Enter path of image: "))
+folder = "Sample-Data/sample house 1"
+files = os.listdir(folder)
 
-try:
+# loop through each image in folder
+for file in files:
+    # ignore hidden files
+    if file.startswith("."):
+        continue
+
+    file_path = os.path.join(folder, file)
+    
     with open(file_path, mode='rb') as image_stream:
         # Call API with image
         results = computervision_client.describe_image_in_stream( image_stream )
         # tags_result_remote = computervision_client.tag_image(remote_image_url)
 
         # Print results with extracted tags
-        print("===== Tags in the remote image =====")
+        print(f"## Tags in the {file} image ##")
         if (len(results.tags) == 0):
             print("No tags detected.")
         else:
             for tag in results.tags:
                 print(tag)
         print()
-except FileNotFoundError:
-    print(f"{os.path.basename(file_path)} file not found")
