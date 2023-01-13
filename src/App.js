108b2a26dd2136a37c213904c545331cb72e80ac
Marco Kaniecki <marcokaniecki@gmail.com>
2023-01-12 23:04:37 -0400

    implemented navigation bar and hero header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Hero from "./components/hero";
 import Navbar from "./components/navbar";
 
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaBars} from 'react-icons/fa'
+import {ImCross} from 'react-icons/im'
 
 const Navbar = () => {
+        const [nav, setNav] = useState(false)
+        const handleClick = () => setNav(!nav)
+
     return (
         <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
             <div className='px-2 flex justify-between items-center w-full h-full'>
                 <div className='flex items-center'>
-                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>NAME.</h1>
+                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>READL.</h1>
+                    <ul className='hidden md:flex'>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Appraisal</li>
+                        <li>Documentation</li>
+                    </ul>
+                </div>
+                <div className='hidden md:flex pr-4'>
+                    <button className='border-none bg-transparent text-black mr-4'>
+                        Some
+                    </button>
+                    <button className='px-8 py-3'>Thing?</button>
+                </div>
+
+                {/* Anything over minimum width the button will be hidden */}
+                <div className='md:hidden' onClick={handleClick}>
+                    {!nav ? <FaBars className='w-5' /> : <ImCross className='w-5' />}
                 </div>
             </div>
-            
-            <FaBars classname='w-5' />
+
+            {/* Drop-dowm menu for mobile sized screens */}
+            <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+                <li className='border-b-2 border-zinc-300 w-full'>Home</li>
+                <li className='border-b-2 border-zinc-300 w-full'>About</li>
+                <li className='border-b-2 border-zinc-300 w-full'>Appraisal</li>
+                <li className='border-b-2 border-zinc-300 w-full'>Documentation</li>
+                
+                {/* Stack buttons ontop of one another using flex and flex-col */}
+                <div className='flex flex-col my-4'>
+                    <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Some</button>
+                    <button className='px-8 py-3'>Thing?</button>
+                </div>
+            </ul>
         </div>
+        
     )
 }
 
