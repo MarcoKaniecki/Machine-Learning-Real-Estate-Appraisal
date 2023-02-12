9f7e336a1d4fbc910a9c82de3a8fb587ee87d355
Marco Kaniecki <marcokaniecki@gmail.com>
2023-02-12 16:28:43 -0400

    moved react files into django frontend app

--- ML_appraisal/frontend/models.py
+++ ML_appraisal/frontend/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class TextInput(models.Model):
+    text = models.TextField()
---
+++ ML_appraisal/frontend/static/components/methodology.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { BsFillPatchCheckFill } from 'react-icons/bs';
+
+const Methodology = () => {
+    return (
+        <div name='methodology' className='w-full my-32'>
+            <div className='max-w-[1240px] mx-auto px-2'>
+                <h2 className='text-5xl font-bold text-center'>Methodology</h2>
+                <p className='text-2xl py-8 text-gray-500 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis omnis eaque eos sunt sit ea consequuntur totam animi recusandae delectus.</p>
+
+                <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+                    <div className='flex'>
+                        <div>
+                            <BsFillPatchCheckFill className='text-indigo-600 w-7 h-7 mr-4'/>  
+                        </div>
+                        
+                        <div>
+                            <h3 className='font-bold text-lg'>Azure Computer Vision</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam quis labore debitis nemo velit impedit!</p>
+                        </div>
+                    </div>
+
+                    <div className='flex'>
+                        <div>
+                            <BsFillPatchCheckFill className='text-indigo-600 w-7 h-7 mr-4'/>  
+                        </div>
+                        
+                        <div>
+                            <h3 className='font-bold text-lg'>Natural Language Processing</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam quis labore debitis nemo velit impedit!</p>
+                        </div>
+                    </div>
+
+                    <div className='flex'>
+                        <div>
+                            <BsFillPatchCheckFill className='text-indigo-600 w-7 h-7 mr-4'/>  
+                        </div>
+                        
+                        <div>
+                            <h3 className='font-bold text-lg'>Support Vector Machine</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam quis labore debitis nemo velit impedit!</p>
+                        </div>
+                    </div>
+
+                    <div className='flex'>
+                        <div>
+                            <BsFillPatchCheckFill className='text-indigo-600 w-7 h-7 mr-4'/>  
+                        </div>
+                        
+                        <div>
+                            <h3 className='font-bold text-lg'>mySQL</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam quis labore debitis nemo velit impedit!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Methodology
