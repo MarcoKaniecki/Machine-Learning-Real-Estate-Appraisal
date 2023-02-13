4532ce35184e9a37d2469386184a8e6200b3619b
Marco Kaniecki <marcokaniecki@gmail.com>
2023-02-13 00:40:49 -0400

    added frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import About from "./components/about";
+import Appraisal from "./pages/appraisal";
+import Footer from "./components/footer";
+import Hero from "./components/hero";
+import Methodology from "./components/methodology";
+import Navbar from "./components/navbar";
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Appraisal />
+      <Methodology />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
