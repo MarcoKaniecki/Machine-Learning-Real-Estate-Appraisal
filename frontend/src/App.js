0dbdd403c602dd6f6cb5e9bae71680beba5edfd6
Marco Kaniecki <marcokaniecki@gmail.com>
2023-02-13 22:48:41 -0400

    integrated and styling for all but appraisal section works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,22 @@
-import React, { Component } from 'react';
-import axios from 'axios';
 
-class App extends Component {
+import About from "./components/about";
+import Appraisal from "./components/appraisal";
+import Footer from "./components/footer";
+import Hero from "./components/hero";
+import Methodology from "./components/methodology";
+import Navbar from "./components/navbar";
 
-  state = {
-    title: '',
-    content: '',
-    image: null
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  };
-
-  handleImageChange = (e) => {
-    this.setState({
-      image: e.target.files[0]
-    })
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(this.state);
-    let form_data = new FormData();
-    form_data.append('image', this.state.image, this.state.image.name);
-    form_data.append('title', this.state.title);
-    form_data.append('content', this.state.content);
-    let url = 'http://localhost:8000/api/posts/';
-    axios.post(url, form_data, {
-      headers: {
-        'content-type': 'multipart/form-data'
-      }
-    })
-        .then(res => {
-          console.log(res.data);
-        })
-        .catch(err => console.log(err))
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <input type="text" placeholder='Title' id='title' value={this.state.title} onChange={this.handleChange} required/>
-          </p>
-          <p>
-            <input type="text" placeholder='Content' id='content' value={this.state.content} onChange={this.handleChange} required/>
-
-          </p>
-          <p>
-            <input type="file"
-                   id="image"
-                   accept="image/png, image/jpeg"  onChange={this.handleImageChange} required/>
-          </p>
-          <input type="submit"/>
-        </form>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Appraisal />
+      <Methodology />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
