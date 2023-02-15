352eb435af46e2aceba648452eee4094881edf53
Marco <85970557+TacticalStack@users.noreply.github.com>
2023-02-14 23:30:19 -0400

    added some styling and removed redundant packages

--- frontend/src/components/appraisal.jsx
+++ frontend/src/components/appraisal.jsx
@@ -1,63 +1,62 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { BsImages, BsCardText, BsSave, BsCloudUpload } from 'react-icons/bs'
-import ApprImg from '../assets/appraisal-bg.png'
+import { useState } from "react";
+import axios from "axios";
 
+const Appraisal = () => {
+  const [image, setImage] = useState(null);
+  const [content, setContent] = useState("");
 
-class appraisal extends Component {
-
-    state = {
-      content: '',
-      image: null
-    };
-  
-    handleChange = (e) => {
-      this.setState({
-        [e.target.id]: e.target.value
-      })
-    };
-  
-    handleImageChange = (e) => {
-      this.setState({
-        image: e.target.files[0]
-      })
-    };
-  
-    handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(this.state);
-      let form_data = new FormData();
-      form_data.append('image', this.state.image, this.state.image.name);
-      form_data.append('content', this.state.content);
-      let url = 'http://localhost:8000/api/posts/';
-      axios.post(url, form_data, {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append("image", image);
+    formData.append("content", content);
+    try {
+      await axios.post("http://localhost:8000/api/posts/", formData, {
         headers: {
-          'content-type': 'multipart/form-data'
-        }
-      })
-          .then(res => {
-            console.log(res.data);
-          })
-          .catch(err => console.log(err))
-    };
-  
-    render() {
-      return (
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <p>
-              <input type="text" placeholder='Content' id='content' value={this.state.content} onChange={this.handleChange} required/>
-            </p>
-            <p>
-              <input type="file"
-                     id="image"
-                     accept="image/png, image/jpeg"  onChange={this.handleImageChange} required/>
-            </p>
-            <button type="submit">Submit!</button>
-          </form>
-        </div>
-      );
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    } catch (error) {
+      console.log(error);
     }
-  }
-  
-  export default appraisal;
+  };
+
+  return (
+    <form className="max-w-xl mx-auto" onSubmit={handleSubmit}>
+      <div className="my-4">
+        <label htmlFor="image" className="block text-gray-700 font-bold mb-2">
+          Image
+        </label>
+        <input
+          type="file"
+          id="image"
+          onChange={(e) => setImage(e.target.files[0])}
+          className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </div>
+      <div className="my-4">
+        <label
+          htmlFor="content"
+          className="block text-gray-700 font-bold mb-2"
+        >
+          Content
+        </label>
+        <textarea
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        ></textarea>
+      </div>
+      <div className="my-4">
+        <input
+          type="submit"
+          value="Submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default Appraisal
