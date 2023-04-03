14253e91071a008d96092b8c7f232060653ea21b
echiasson8 <erinchiasson8@gmail.com>
2023-04-03 16:02:43 -0300

    Added test for compextraction, added support for multithreading

---
+++ backend/Tests/CompExtractionTest.py
@@ -0,0 +1,41 @@
+from CompDatabase import CompExtraction
+
+test_input = {
+    "id": 1,
+    "area": 1656,
+    "zone": "RL",
+    "lotArea": 31770,
+    "utilities": "AllPub",
+    "neighborhood": "NAmes",
+    "bldgType": "1Fam",
+    "houseStyle": "1Story",
+    "overallQual": "6",
+    "overallCond": "5",
+    "yearBuilt": 1960,
+    "yearRemod": 1960,
+    "exterior1": "BrkFace",
+    "exterQual": "TA",
+    "exterCond": "TA",
+    "foundation": "CBlock",
+    "bsmtFinType1": "BLQ",
+    "bsmtFindSF1": 639,
+    "totalBsmtSF": 1080,
+    "heating": "GasA",
+    "heatingQC": "Fa",
+    "centralAir": "Y",
+    "electrical": "SBrkr",
+    "fullBath": 1,
+    "halfBath": 0,
+    "bedroom": 3,
+    "kitchen": 1,
+    "kitchenQual": "TA",
+    "totRmsAbvGrd": 7,
+    "garageType": "Attchd",
+    "garageCars": 2,
+    "garageArea": 528,
+    "garageQual": "TA",
+    "woodDeckSF": 210,
+    "fence": "NA"
+}
+
+comps = CompExtraction.FindComps(test_input)
